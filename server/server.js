b6de635440fb214508d85f09903b58187867e3ba
Sach Denuwara <sddenuwara@gmail.com>
2023-12-11 21:29:31 -0500

    CORS FIX 6

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = 3000;
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://seahorse-app-sxjhm.ondigitalocean.app');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Credentials', true);
