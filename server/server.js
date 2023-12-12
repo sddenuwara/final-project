e96b81788cafe2f69fa5a4fb34916f710c46c94b
Sach Denuwara <sddenuwara@gmail.com>
2023-12-11 21:07:30 -0500

    CORS Fix 3

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const port = 3000;
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://seahorse-app-sxjhm.ondigitalocean.app');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 })
 
